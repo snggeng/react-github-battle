8f6395665ee61f8f6332dc6745d338086904ea95
Geng <snggeng@gmail.com>
2017-06-16 13:02:26 +0800

    added index.html, webpack.config.js, configured package.json and index.js to create new component

---
+++ app/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+require('./app/index.css')
+
+// State
+// Lifecycle Event
+// UI
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+      Hello World!
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
