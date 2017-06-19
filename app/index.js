418b79dfe9eb58e708f5e0d174cc642ec289a68d
Geng <snggeng@gmail.com>
2017-06-19 22:27:22 +0800

    added badge component in index.js

--- app/index.js
+++ app/index.js
@@ -6,17 +6,27 @@ require('./index.css')
 // Lifecycle Event
 // UI
 
-class App extends React.Component {
+class Badge extends React.Component {
   render () {
     return (
       <div>
-      Hello World!
+        <img
+          src={this.props.img}
+          alt='Avatar'
+          style={{width: 100, height: 100}}
+        />
+        <h1>Name: {this.props.name}</h1>
+        <h3>Username: {this.props.username}</h3>
       </div>
     )
   }
 }
 
 ReactDOM.render(
-  <App />,
+  <Badge
+    name='Geng Sng'
+    username='snggeng'
+    img={'https://avatars0.githubusercontent.com/u/293340?v=3&s=460'}
+  />,
   document.getElementById('app')
 )
