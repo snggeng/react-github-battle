d29743019c94b67a6c31961d251dadee65c83e6d
Geng <snggeng@gmail.com>
2017-06-21 09:18:55 +0800

    changed index.js to act as the main entry point for component rendering, so all future components will be imported and rendered in index.js | no components created here

--- app/index.js
+++ app/index.js
@@ -1,32 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 require('./index.css')
+import App from './components/App'
 
 // State
 // Lifecycle Event
 // UI
 
-class Badge extends React.Component {
-  render () {
-    return (
-      <div>
-        <img
-          src={this.props.img}
-          alt='Avatar'
-          style={{width: 100, height: 100}}
-        />
-        <h1>Name: {this.props.name}</h1>
-        <h3>Username: {this.props.username}</h3>
-      </div>
-    )
-  }
-}
-
 ReactDOM.render(
-  <Badge
-    name='Geng Sng'
-    username='snggeng'
-    img={'https://avatars0.githubusercontent.com/u/293340?v=3&s=460'}
-  />,
+  <App />,
   document.getElementById('app')
 )
