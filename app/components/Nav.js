c53b8e06d287adfe518354007111e9fd69eb5407
Geng <snggeng@gmail.com>
2017-06-29 01:35:42 +0800

    completed Nav.js stateless function

---
+++ app/components/Nav.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactRouter from 'react-router-dom'
+const NavLink = ReactRouter.NavLink
+
+const Nav = () => {
+  return (
+    <ul className='nav'>
+      <li>
+        <NavLink exact activeClassName='active' to='/'>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName='active' to='/battle'>
+          Battle
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName='active' to='/popular'>
+          Popular
+        </NavLink>
+      </li>
+    </ul>
+  )
+}
+
+module.exports = Nav
