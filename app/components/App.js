da8453b93473844a0229e8020304b724be19999d
Geng <snggeng@gmail.com>
2017-06-29 01:30:11 +0800

    npm i react-router-dom and updated app.js to have route for popular

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import ReactRouter from 'react-router-dom'
+const Router = ReactRouter.BrowserRouter
+const Route = ReactRouter.Route
+
+// Import Components
 import Popular from './Popular'
 
 class App extends React.Component {
   render () {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+      <Router>
+        <div className='container'>
+          <Route path='/popular' component={Popular} />
+        </div>
+      </Router>
+
     )
   }
 }
