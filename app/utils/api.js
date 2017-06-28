5411bd8b0a6d97b60b14237fe74228f723e2a585
Geng <snggeng@gmail.com>
2017-06-29 00:44:59 +0800

    added axios get to api.js

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,5 +1,12 @@
 import axios from 'axios'
 
 module.exports = {
-  
+  fetchPopularRepos: (language) => {
+    let encodedURI = window.encodeURI(`https://api.github.com/search/repositories?
+      q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
+  return axios.get(encodedURI) // returns a promise
+    .then((response) => {
+      return response.data.items
+    })
+  }
 }
